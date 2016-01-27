a74e0707dffeacdca239dc293fe71a9e954b4da0
EricRiney <fails.to.install@gmail.com>
2016-01-27 10:03:39 -0800

    meow

--- node-db/models/story.js
+++ node-db/models/story.js
@@ -8,6 +8,21 @@ var Story = {
         var sql = `select * from stories 
             order by votes desc, createdOn desc limit 50`;
         return connPool.queryAsync(sql);
+    },
+    
+    insert(story) {
+        //validate data
+        var sql = "insert into stories (url) and values (?)";
+        var params = [story.url];
+        return connPool.queryAsync(sql, params)
+            .then(function(results) {
+                sql = 'SELECT * FROM stories WHERE id=?';
+                params = [results.insertID];
+                return connPool.queryAsync(sql, params);
+            })
+            .then(function(rows) {
+                return rows.length > 0 ? rows[0] : null;
+            })
     }
 };
 
